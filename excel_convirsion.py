d3b1e2c79f58c03b5ffa174dde9a3e79ac84d95d
Mizuki Shindo <universe.1364.m.s@gmail.com>
2025-06-08 19:31:27 +0900

    name and save file

--- excel_convirsion.py
+++ excel_convirsion.py
@@ -1,9 +1,13 @@
 import tkinter as tk #GUI構築ライブラリ
-from tkinter import filedialog #ファイル選択ダイアログ
+from tkinter import filedialog, messagebox  #ファイル選択ダイアログ
 import openpyxl as px #pythonで.xlsxを開く追加ライブラリ（インストール済み）
-# import xlrd as px #pythonで.xlsを開く追加ライブラリ（インストール済み）
+
+#グローバル変数としてワークブックを保持する
+wb = None
+
 
 def open_file():
+    global wb
     file_path = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx *.xls")])
 
     if file_path:
@@ -13,10 +17,25 @@ def open_file():
         # 例: 最初のシート名を表示
         sheet_names = wb.sheetnames
         print(f"シート一覧: {sheet_names}")
+        messagebox.showinfo("ファイル読み込み完了", "ファイルを正常に読み込みました^^")
+
+def save_file():
+    global wb
+    if wb is None:
+        messagebox.showwarning("エラー", "まずExcelファイル(*.xlsxまたは*.xls)を開いてください😡")
+        return
+    
+    save_path = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=[("Excel files", "*.xlsx")], title = "名前を付けて保存")
+
+    if save_path:
+        wb.save(save_path)
+        messagebox.showinfo("保存完了", f"Excelファイルを保存しました:\n{save_path}")
+        print(f"ファイルを保存しました: {save_path}")
+    
 
 # GUIウィンドウ作成
 root = tk.Tk()
-root.title("Excelデータ変換アプリ")
+root.title("Excelデータ変換")
 
 #概要を表示する
 description = tk.Label(root, text="Excelファイルを読み込み、単位変換とグラフ描画を行います(´・ω・｀)", font=("Helvetica", 9))
@@ -26,8 +45,14 @@ description.pack(padx = 10, pady =(10, 5))
 open_button = tk.Button(root, text="Excelファイルを開く", command=open_file)
 open_button.pack(padx=10, pady=10)
 
+#「名前をつけて保存」ボタン
+save_button = tk.Button(root, text="保存先を指定して保存", command=save_file)
+save_button.pack(padx=20, pady=10)
+
+#「アプリを終了する」ボタン
+quit_button = tk.Button(text = "アプリを終了する", command = root.destroy)
+quit_button.pack(padx=30, pady=10)
+
+
+
 root.mainloop()
-
-#wb = plx.load_workbook("test.xlsx") #wbに指定ワークブックを入れる
-
-#wb.save("../test2.xlsx") #指定ワークブックにファイルパスを与えて保存
